32e252d405a5737fa68d00e663b7b9c35f7edce0
Tuğçe Karakaya <tugcekarakaya90@gmail.com>
2022-08-09 11:21:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/harita_denemesi/MainActivity.java
@@ -0,0 +1,160 @@
+package com.example.harita_denemesi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    SupportMapFragment mapFragment;
+    FusedLocationProviderClient client;//geçerli konumu almak için kullanılır.
+    GoogleMap map;
+    SearchView searchView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchView=findViewById(R.id.search_view);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        //Konum izinlerinin alınması
+        Dexter.withContext(getApplicationContext())
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                getmyLocation();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {//searchview içindeki kullanıcı eylemleri için bir dinleyici ayarlar.
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String adresName = searchView.getQuery().toString();
+                List<Address> addressList=null;
+                if(adresName != null || !adresName.equals("")){
+                    Geocoder geocoder = new Geocoder((MainActivity.this));
+                    try{
+                        addressList = geocoder.getFromLocationName(adresName,10);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (addressList != null && addressList.size()>0) {
+                        Address address = addressList.get(0);
+                        LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                        map.addMarker(new MarkerOptions().position(latLng).title(adresName + ": " + address.getFeatureName() + "/" + address.getCountryName()));
+                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+                    } else {
+                        //alert
+                        Toast.makeText(MainActivity.this, "adres bulunamadı", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        mapFragment.getMapAsync(this);
+    }
+
+
+    public void getmyLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                mapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(@NonNull GoogleMap googleMap) {
+                        LatLng latlng= new LatLng(location.getLatitude(),location.getLongitude());
+                        MarkerOptions markerOptions = new MarkerOptions().position(latlng).title("Buradasınız");
+
+                        Geocoder geocoder = new Geocoder((MainActivity.this));
+
+                        try {
+                            List<Address> addressList = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                            Log.d("getCountryName", addressList.get(0).getCountryName());
+                            Log.d("getFeatureName", addressList.get(0).getAdminArea());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        googleMap.addMarker(markerOptions);//konumu işaretçi olarak ekrana işaretler.
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng,15));
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map=googleMap;
+    }
+}
